9f6b85d585712bd2c148d9bce5275b0d0c2c0e2d
Paul Morris <creative2012@hotmail.com>
2023-01-06 21:17:42 +0000

    cleanded code and comments

--- script.js
+++ script.js
@@ -87,87 +87,19 @@ var upperCasedCharacters = [
   'Y',
   'Z'
 ];
-//length of character type arrays so as not to need to do it on each char
-const lowerCasedCharactersLength = lowerCasedCharacters.length;
-const upperCasedCharactersLength = upperCasedCharacters.length;
-const numericCharactersLength = numericCharacters.length;
-const specialCharactersLength = specialCharacters.length;
-//Function to assign characterType options from input
-function getCharTypes() {
+//length of character type arrays (used every character call)
+const lowerCasedCharactersLength = lowerCasedCharacters.length -1;
+const upperCasedCharactersLength = upperCasedCharacters.length -1;
+const numericCharactersLength = numericCharacters.length -1; 
+const specialCharactersLength = specialCharacters.length -1;
+//prompts
+const pLength = "What length would you like your password? (between 10 and 64)";
+const pLengthError = "**Incorrect entry** \nPlease re-enter a number between 10 and 64 only";
+const pcharTypes = "What length would you like your password? (between 10 and 64)";
+const pCharTypeError = "**Please select atleast 1 option** \nWhat character types would you like in your password?\n Enter all required into the box from the options below\n ( L = lowercase, U = uppercase, N = numeric, S = special characters )";
+const pConfirm1 = 'Are these options correct ?\n\n' ;
+const pConfirm2 = '\n\nIf not please click cancel to select again';
 
-  var types = []
-
-  //prompt user
-  var characterTypes = prompt("What character types would you like in your password?\n Enter all required options into the box together from the options below\n ( L = lowercase, U = uppercase, N = numeric, S = special characters )");
-
-  //check an option has been selected
-  const chars = /[lL]|[uU]|[nN]|[sS]/g;
-  var found = characterTypes.match(chars);
-
-  //if incorrect entry re-prompt user
-  while (found == null) {
-    characterTypes = prompt("**Please select atleast 1 option** \nWhat character types would you like in your password?\n Enter all required into the box from the options below\n ( L = lowercase, U = uppercase, N = numeric, S = special characters )");
-    found = characterTypes.match(chars);
-  }
-
-  //assign matches to true in types object if found, false if not
-  found = found.toString().toUpperCase();
-  found.includes('L') ? types.push('LowerCase') : '';
-  found.includes('U') ? types.push('UpperCase') : '';
-  found.includes('N') ? types.push('Numeric') : '';
-  found.includes('S') ? types.push('Special') : '';
-
-
-  //present keys to check if user is happy with selection, just incase user spelt out entire word instead of just a letter and we detected other options by mistake
-  var confirmBox = confirm('Are these options correct ?\n\n' + types.toString().replace(/,/g, ' - ') + '\n\nIf not please click cancel to select again');
-
-  //if not, go again
-  if (!confirmBox) {
-    types = getCharTypes();
-  }
-
-  return types;
-}
-
-// Function to prompt user for password options
-function getPasswordOptions() {
-
-  var options = {
-    length: 0,
-    characterTypes: []
-  }
-
-  //prompt user
-  var length = prompt("What length would you like your password? (between 10 and 64)");
-
-  //Check correct entry
-  while (length < 10 || length > 64 || isNaN(length)) {
-    length = prompt("**Incorrect entry** \nPlease re-enter a number between 10 and 64 only")
-  }
-  options.length = length;
-  //prompt user for character types
-  options.characterTypes = getCharTypes();
-
-  return options;
-}
-
-// Function for getting a random element from an array
-function getRandom(arr) {
-
-  if (arr == 'LowerCase') {
-    return lowerCasedCharacters[Math.floor(Math.random() * (lowerCasedCharactersLength - 1))];
-  }
-  if (arr == 'UpperCase') {
-    return upperCasedCharacters[Math.floor(Math.random() * (upperCasedCharactersLength - 1))];
-  }
-  if (arr == 'Numeric') {
-    return numericCharacters[Math.floor(Math.random() * (numericCharactersLength - 1))];
-  }
-  if (arr == 'Special') {
-    return specialCharacters[Math.floor(Math.random() * (specialCharactersLength - 1))];
-  }
-
-}
 //function to shuffle array ref: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 function shuffle(array) {
   let currentIndex = array.length, randomIndex;
@@ -187,20 +119,87 @@ function shuffle(array) {
   return array;
 }
 
+//Function to assign characterType options from input
+function getCharTypes() {
+
+  var types = []
+  var characterTypes = prompt(pcharTypes); //prompt user for character types
+
+  //check an option has been selected
+  const chars = /[lL]|[uU]|[nN]|[sS]/g;
+  var found = characterTypes.match(chars);
+
+  //if incorrect entry re-prompt user
+  while (found == null) {
+    characterTypes = prompt(pCharTypeError); //promot again with error msg
+    found = characterTypes.match(chars);
+  }
+
+  //assign matches to true in types object if found, false if not
+  found = found.toString().toUpperCase();
+  found.includes('L') ? types.push('LowerCase') : '';
+  found.includes('U') ? types.push('UpperCase') : '';
+  found.includes('N') ? types.push('Numeric') : '';
+  found.includes('S') ? types.push('Special') : '';
+
+
+  //present keys to check if user is happy with selection, just incase user spelt out entire word instead of just a letter and we detected other options by mistake
+  var confirmBox = confirm(pConfirm1 + types.toString().replace(/,/g, ' - ') + pConfirm2);
+
+  //if not, go again
+  if (!confirmBox) {
+    types = getCharTypes();
+  }
+
+  return types;
+}
+
+// Function to prompt user for password options
+function getPasswordOptions() {
+
+  var options = {
+    length: 0,
+    characterTypes: []
+  }
+
+  var length = prompt(pLength); //prompt user for length of password
+
+  //Check correct entry
+  while (length < 10 || length > 64 || isNaN(length)) {
+    length = prompt(pLengthError) //prompt again with error msg
+  }
+  
+  options.length = length; //set length
+  options.characterTypes = getCharTypes(); // run function to prompt  user for character types and set
+
+  return options;
+}
+
+// Function for getting a random element from an array
+function getRandom(arr) {
+
+  var char = '';
+
+  arr == 'LowerCase' ? char = lowerCasedCharacters[Math.floor(Math.random() * lowerCasedCharactersLength)] : '';
+  arr == 'UpperCase' ? char = upperCasedCharacters[Math.floor(Math.random() * upperCasedCharactersLength)] : '';
+  arr == 'Numeric' ? char = numericCharacters[Math.floor(Math.random() * numericCharactersLength)] : '';
+  arr == 'Special' ? char = specialCharacters[Math.floor(Math.random() * specialCharactersLength)] : '';
+  
+  return char;
+
+}
+
 // Function to generate password with user input
 function generatePassword() {
   var password = [];
   var options = getPasswordOptions();
 
-  var charTypeLength = options.characterTypes.length;
-  var optionsLength = options.length
-  //times to add each type of character type based on user chosen length and number of options
-  var characterTimes = Math.floor(optionsLength / (charTypeLength));
-  //remaining times to add characters if any left over from the above calc
-  var remainingChar = optionsLength % (options.characterTypes.length);
+  var charTypeLength = options.characterTypes.length;  //length of character options selected (used twice)
+  var optionsLength = options.length //length password needs to be (used twice)
+  var characterTimes = Math.floor(optionsLength / (charTypeLength)); //times to add each type of character type based on user chosen length and number of options
+  var remainingChar = optionsLength % (options.characterTypes.length); //remaining times to add characters if any left over from the above calc
 
   //get random characters for each user option character type
-  
   options.characterTypes.forEach(function (x) {
     for (var i = 0; i < characterTimes; i++) {
       var option = x;
@@ -212,19 +211,17 @@ function generatePassword() {
   //if the count of options divided by the length of the password has a remainer, add the extra characters here
   if (remainingChar > 0) {
     while (remainingChar > 0) {
-      //for the remaining characters choose random type from user options
-      var option = options.characterTypes[Math.floor(Math.random() * (charTypeLength - 1))]
+      var option = options.characterTypes[Math.floor(Math.random() * (charTypeLength - 1))]; //for the remaining characters choose random type from user options
       password.push(getRandom(option));
       remainingChar --;
     }
   }
 
-  // shuffle passowrd and return as string
-  return shuffle(password).toString().replace(/,/g, '');
+  return shuffle(password).toString().replace(/,/g, ''); // shuffle passowrd and return as string
 }
 
-// Get references to the #generate element
-var generateBtn = document.querySelector('#generate');
+
+var generateBtn = document.querySelector('#generate'); // Get references to the #generate element
 
 // Write password to the #password input
 function writePassword() {
